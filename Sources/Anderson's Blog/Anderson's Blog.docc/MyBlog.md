b3169187b15e2cf4e747d48bc130d0f1bc8f0935
Anderson <samadhihuang@gmail.com>
2025-06-14 11:55:56 +0800

    deploy blog

---
+++ Sources/Anderson's Blog/Anderson's Blog.docc/MyBlog.md
@@ -0,0 +1,7 @@
+#  ``Anderson_s_Blog``
+
+Welcome to my blog!
+My name is Anderson, an iOS/macOS developer. Code with Swift and Haskell is my favorite!
+
+## Content
+- <doc:welcome>
---
+++ Tests/Anderson-s-BlogTests/Anderson_s_BlogTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import Anderson_s_Blog
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
